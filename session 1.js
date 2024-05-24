5f399b8093ae568395b38d81d1f176948584ca13
Danialzf <dzanjanifard@gmail.com>
2024-05-24 12:23:13 +0330

    first commit

---
+++ session 1.js
@@ -0,0 +1,140 @@
+// console.log('fekre new instititution')
+
+// alert('mr danial')
+
+// document.write('danial zanjanifard')
+
+// let x ='danial'
+// let y = 'znjanifard'
+
+// console.log(x)
+// console.log(x,y)
+// console.log(y)
+
+// let x = 'danial'
+// console.log(x);
+
+// var danial = 'fard'
+// console.log(danial);
+// var danial = 'fekre'
+// console.log(danial);
+// var danial = 'new'
+// console.log(danial);
+
+
+// const country= 'iran'
+// console.log(country)
+
+// const country= 'usa'
+// console.log(country)
+
+// let f ='saman'
+// console.log(f);
+
+// var g = 'aslani'
+
+// console.log(f,g)
+
+// {
+//     let f = 'wahid'
+//     var g = 'emami'
+//     console.log(f,g);
+// }
+
+// console.log(f,g)
+
+console.log(zanjani);
+var zanjani ='danial';
+console.log(zanjani);
+// console.log(zanjani)
+// let zanjani ='danial'
+// console.log(zanjani)
+// مورد بالایی اشتباه است
+
+// let 8saman = 'saman'
+
+// let danial zanjani = 'danial'
+
+// let danial-zanjani='danial'
+
+let danial_zanjani='danial';
+let danialZanjani='zanjani';
+
+// let else=''
+
+let x = 1;
+console.log(x);
+console.log(typeof x);
+console.log(typeof (x));
+
+let y ='2';
+console.log(y);
+console.log(typeof y);
+console.log(typeof (y));
+
+let firstName = 'danial';
+console.log(firstName);
+console.log(typeof firstName);
+console.log(typeof (firstName));
+
+// let poya = '';
+// console.log(poya);
+// console.log(typeof poya);
+
+// let poya ;
+// console.log(poya);
+// console.log(typeof poya);
+
+let poya = null;
+console.log(poya)
+console.log(typeof poya);
+// boolean
+pardis = true;
+console.log(pardis);
+console.log(typeof pardis);
+
+poya = false;
+console.log(poya);
+console.log(typeof poya);
+poya = 'false';
+console.log(poya);
+console.log(typeof poya);
+
+let cars = [206 , 'peykan' , 'toyota', 'bmw',5,true]
+console.log(cars);
+console.log(typeof poya);
+
+let students={
+    firstName: 'danial',
+    lastName: 'zanjani'
+
+
+}
+
+console.log(students)
+console.log(typeof students);
+
+let z=5;
+// z = z+1;
+// z+=1;
+z++;
+console.log(z);
+
+let c=10;
+// c+=2;
+// c=c+2;
+console.log(c);
+
+let v=5;
+let b=6;
+v=v+b;
+console.log(v);
+console.log(b);
+
+
+
+
+
+
+
+
